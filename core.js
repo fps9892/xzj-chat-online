0ce7797537d6d94fa9ed71d402382420c3e84387
Fabian <peraltafabian380@gmail.com>
2025-11-28 15:20:18 -0300

    mensaje de actualizacion Fluxion

--- core.js
+++ core.js
@@ -78,6 +78,7 @@ export async function getUserProfile(userId, isGuest = false) {
             lastSeen: userData.lastSeen || userData.lastUpdated || 'No disponible',
             avatar: userData.avatar || 'images/profileuser.jpg',
             textColor: userData.textColor || '#ffffff',
+            level: userData.level || 1,
             messageCount: userData.messageCount || 0,
             isGuest, isAdmin, isModerator, isBanned
         };
