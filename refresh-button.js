29993188f686e301796b6cea249a353b1eb1ae07
Fabian <peraltafabian380@gmail.com>
2025-12-01 04:20:18 -0300

    mensaje de actualizacion Fluxion

---
+++ refresh-button.js
@@ -0,0 +1,24 @@
+let scrollTimeout;
+let lastScrollY = 0;
+
+const refreshBtn = document.getElementById('refreshBtn');
+const chatArea = document.querySelector('.chat-area');
+
+chatArea.addEventListener('scroll', () => {
+  const currentScrollY = chatArea.scrollTop;
+  
+  if (currentScrollY > lastScrollY && currentScrollY > 100) {
+    refreshBtn.classList.add('show');
+    
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+      refreshBtn.classList.remove('show');
+    }, 10000);
+  }
+  
+  lastScrollY = currentScrollY;
+});
+
+refreshBtn.addEventListener('click', () => {
+  location.reload();
+});
